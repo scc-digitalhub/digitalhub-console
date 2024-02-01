c4d0504bbbaf1b2a166f36f99132efe6f4808a42
Claudio Ballardini <claudio.ballardini01@gmail.com>
2024-02-01 12:47:02 +0100

    Add prettier configuration, add MUI X Data Grid, change version box

--- src/components/helper.tsx
+++ src/components/helper.tsx
@@ -1,161 +1,168 @@
-import { BackButton } from "@dslab/ra-back-button";
-import { ExportRecordButton } from "@dslab/ra-export-record-button";
-import { InspectButton } from "@dslab/ra-inspect-button";
-import { useRootSelector } from "@dslab/ra-root-selector";
-import ClearIcon from "@mui/icons-material/Clear";
-import FileCopyIcon from "@mui/icons-material/FileCopy";
+import { BackButton } from '@dslab/ra-back-button';
+import { ExportRecordButton } from '@dslab/ra-export-record-button';
+import { InspectButton } from '@dslab/ra-inspect-button';
+import { useRootSelector } from '@dslab/ra-root-selector';
+import ClearIcon from '@mui/icons-material/Clear';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
 import {
-  Box,
-  Card,
-  CardContent,
-  CardHeader,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-} from "@mui/material";
-import { ReactNode, memo, useEffect, useState } from "react";
+    Box,
+    Card,
+    CardContent,
+    CardHeader,
+    Table as MaterialTable,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+} from '@mui/material';
+import { ReactNode, memo, useEffect, useState } from 'react';
 import {
-  Button,
-  ButtonProps,
-  Datagrid,
-  DateField,
-  DeleteWithConfirmButton,
-  EditButton,
-  List,
-  Pagination,
-  RaRecord,
-  SaveButton,
-  ShowButton,
-  TextField,
-  Toolbar,
-  TopToolbar,
-  useDataProvider,
-  useRecordContext,
-  useResourceContext,
-  useTranslate,
-} from "react-admin";
-import { useNavigate } from "react-router-dom";
+    Button,
+    ButtonProps,
+    Datagrid,
+    DateField,
+    DeleteWithConfirmButton,
+    EditButton,
+    List,
+    Pagination,
+    RaRecord,
+    SaveButton,
+    ShowButton,
+    TextField,
+    Toolbar,
+    TopToolbar,
+    useDataProvider,
+    useRecordContext,
+    useResourceContext,
+    useTranslate,
+} from 'react-admin';
+import { useNavigate } from 'react-router-dom';
+import Grid from '@mui/material/Grid';
 
 export const RecordTitle = ({ prompt }: any) => {
-  const record = useRecordContext();
-  return (
-    <span>
-      {prompt} {record ? `${record.name}` : ""}
-    </span>
-  );
+    const record = useRecordContext();
+    return (
+        <span>
+            {prompt} {record ? `${record.name}` : ''}
+        </span>
+    );
 };
 
 export const DeleteWithConfirmButtonShowingName = () => {
-  const record = useRecordContext();
-  if (!record) return <></>;
-  return <DeleteWithConfirmButton translateOptions={{ id: record.name }} />;
+    const record = useRecordContext();
+    if (!record) return <></>;
+    return <DeleteWithConfirmButton translateOptions={{ id: record.name }} />;
 };
 
 export const NewVersionButton = (props: ButtonProps) => {
-  const navigate = useNavigate();
-  const { label = "buttons.newVersion", ...rest } = props;
-  return (
-    <Button label={label} onClick={() => navigate("update")} {...rest}>
-      <FileCopyIcon />
-    </Button>
-  );
+    const navigate = useNavigate();
+    const { label = 'buttons.newVersion', ...rest } = props;
+    return (
+        <Button label={label} onClick={() => navigate('update')} {...rest}>
+            <FileCopyIcon />
+        </Button>
+    );
 };
 
 export const PostShowActions = () => {
-  const record = useRecordContext();
-  return <ShowToolbar record={record} />;
+    const record = useRecordContext();
+    return <ShowToolbar record={record} />;
 };
 
 const arePropsEqual = (oldProps: any, newProps: any) => {
-  if (!newProps.record) return true;
-  return Object.is(oldProps.record, newProps.record);
+    if (!newProps.record) return true;
+    return Object.is(oldProps.record, newProps.record);
 };
 
 const ShowToolbar = memo(function ShowToolbar(props: { record: any }) {
-  if (!props.record) return <></>;
-  return (
-    <TopToolbar>
-      <EditButton style={{ marginLeft: "auto" }} record={props.record} />
-      <InspectButton record={props.record} />
-      <ExportRecordButton language="yaml" record={props.record} />
-      <DeleteWithConfirmButton
-        translateOptions={{ id: props.record.name }}
-        record={props.record}
-      />
-    </TopToolbar>
-  );
+    if (!props.record) return <></>;
+    return (
+        <TopToolbar>
+            <BackButton sx={{ display: { xs: 'none', lg: 'flex' } }} />
+            <EditButton style={{ marginLeft: 'auto' }} record={props.record} />
+            <InspectButton record={props.record} />
+            <ExportRecordButton language="yaml" record={props.record} />
+            <DeleteWithConfirmButton
+                translateOptions={{ id: props.record.name }}
+                record={props.record}
+            />
+        </TopToolbar>
+    );
 }, arePropsEqual);
 
 export const ListVersion = () => {
-  const record = useRecordContext();
-  const resource = useResourceContext();
-  const dataProvider = useDataProvider();
-  const { root } = useRootSelector();
-  const translate = useTranslate();
-  const [versions, setVersions] = useState<RaRecord>();
+    const record = useRecordContext();
+    const resource = useResourceContext();
+    const dataProvider = useDataProvider();
+    const { root } = useRootSelector();
+    const translate = useTranslate();
+    const [versions, setVersions] = useState<RaRecord>();
 
-  useEffect(() => {
-    if (dataProvider) {
-      dataProvider.getLatest(resource, { record, root }).then((versions) => {
-        setVersions(versions.data);
-      });
-    }
-  }, [dataProvider, record, resource]);
+    useEffect(() => {
+        if (dataProvider) {
+            dataProvider
+                .getLatest(resource, { record, root })
+                .then(versions => {
+                    setVersions(versions.data);
+                });
+        }
+    }, [dataProvider, record, resource]);
 
-  if (!versions || !record || !dataProvider) return <></>;
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell align="center">
-            {translate("resources.list.expandable.version")}
-          </TableCell>
-          <TableCell align="center">
-            {translate("resources.list.expandable.created")}
-          </TableCell>
-          <TableCell></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {versions.map((item) => (
-          <TableRow key={item.id}>
-            <TableCell component="th" scope="row" align="center">
-              {item.metadata?.version}
-            </TableCell>
-            <TableCell align="center">
-              <DateField source="created" record={item.metadata} />
-            </TableCell>
-            <TableCell size="small" align="right">
-              <ShowButton record={item} />
-            </TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  );
+    if (!versions || !record || !dataProvider) return <></>;
+    return (
+        <MaterialTable>
+            <TableHead>
+                <TableRow>
+                    <TableCell align="center">
+                        {translate('resources.list.expandable.version')}
+                    </TableCell>
+                    <TableCell align="center">
+                        {translate('resources.list.expandable.created')}
+                    </TableCell>
+                    <TableCell></TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {versions.map(item => (
+                    <TableRow key={item.id}>
+                        <TableCell component="th" scope="row" align="center">
+                            {item.metadata?.version}
+                        </TableCell>
+                        <TableCell align="center">
+                            <DateField
+                                source="created"
+                                record={item.metadata}
+                            />
+                        </TableCell>
+                        <TableCell size="small" align="right">
+                            <ShowButton record={item} />
+                        </TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </MaterialTable>
+    );
 };
 
 export const PostEditToolbar = () => {
-  const translate = useTranslate();
-  const navigate = useNavigate();
-  const handleClick = () => {
-    navigate(-1);
-  };
-  return (
-    <Toolbar>
-      <SaveButton />
-      <Button
-        color="info"
-        label={translate("buttons.cancel")}
-        onClick={handleClick}
-      >
-        <ClearIcon />
-      </Button>
-      <NewVersionButton />
-    </Toolbar>
-  );
+    const translate = useTranslate();
+    const navigate = useNavigate();
+    const handleClick = () => {
+        navigate(-1);
+    };
+    return (
+        <Toolbar>
+            <SaveButton />
+            <Button
+                color="info"
+                label={translate('buttons.cancel')}
+                onClick={handleClick}
+            >
+                <ClearIcon />
+            </Button>
+            <NewVersionButton />
+        </Toolbar>
+    );
 };
 export const TaskToolbar = () => {
   return (
@@ -165,130 +172,122 @@ export const TaskToolbar = () => {
   );
 };
 const getStyle = (record: any) => {
-  const curRecord = record;
+    const curRecord = record;
 
-  return {
-    rowSx: (record) => ({
-      backgroundColor:
-        record.id === curRecord.id ? "rgb(25, 118, 210, 0.15)" : "white",
-      ":hover": {
-        backgroundColor:
-          record.id === curRecord.id
-            ? "rgb(25, 118, 210, 0.25) !important"
-            : "rgba(0, 0, 0, 0.04)",
-      },
-    }),
-  };
+    return {
+        rowSx: record => ({
+            backgroundColor:
+                record.id === curRecord.id
+                    ? 'rgb(25, 118, 210, 0.15)'
+                    : 'white',
+            ':hover': {
+                backgroundColor:
+                    record.id === curRecord.id
+                        ? 'rgb(25, 118, 210, 0.25) !important'
+                        : 'rgba(0, 0, 0, 0.04)',
+            },
+        }),
+    };
 };
 
 export const LayoutContent = (props: LayoutContentProps) => {
-  const { children, record } = props;
-  const dataProvider = useDataProvider();
-  const translate = useTranslate();
+    const { children, record } = props;
+    const dataProvider = useDataProvider();
+    const translate = useTranslate();
 
-  if (!dataProvider) return <></>;
-  return (
-    <Box
-      sx={(theme) => ({
-        display: "flex",
-        flexDirection: "column",
-        [theme.breakpoints.up("lg")]: {
-          flexDirection: "row",
-        },
-      })}
-    >
-      <Box display="block">
-        <Box
-          display="flex"
-          flexDirection="column"
-          sx={(theme) => ({
-            paddingRight: 0,
-            [theme.breakpoints.up("lg")]: {
-              paddingRight: 4,
-            },
-          })}
+    if (!dataProvider) return <></>;
+    return (
+        <Grid
+            container
+            spacing={4}
+            direction={{ xs: 'column-reverse', lg: 'row' }}
+            flexWrap={{ xs: 'nowrap', lg: 'wrap' }}
         >
-          <TopToolbar sx={{ justifyContent: "flex-start" }}>
-            <BackButton />
-          </TopToolbar>
+            <Grid item xs={12} lg={6} xl={7} pb={2}>
+                {children}
+            </Grid>
 
-          <Card
-            sx={{
-              minWidth: { lg: 500, xl: 550 },
-              height: "fit-content",
-              borderRadius: "10px",
-            }}
-            variant="outlined"
-          >
-            <CardHeader title={translate("resources.common.version.title")} />
+            <Grid item xs={12} lg={6} xl={5}>
+                <TopToolbar sx={{ justifyContent: 'flex-start' }}>
+                    <BackButton sx={{ display: { xs: 'flex', lg: 'none' } }} />
+                </TopToolbar>
 
-            <CardContent
-              sx={{ paddingTop: 1, paddingBottom: "8px !important" }}
-            >
-              <BoxContent record={record} />
-            </CardContent>
-          </Card>
-        </Box>
-      </Box>
-      
-      {children}
-    </Box>
-  );
+                <Card
+                    sx={{
+                        height: 'fit-content',
+                        borderRadius: '10px',
+                    }}
+                    variant="outlined"
+                >
+                    <CardHeader
+                        title={translate('resources.common.version.title')}
+                    />
+
+                    <CardContent
+                        sx={{
+                            paddingTop: 1,
+                            paddingBottom: '8px !important',
+                        }}
+                    >
+                        <BoxContent record={record} />
+                    </CardContent>
+                </Card>
+            </Grid>
+        </Grid>
+    );
 };
 
-const BoxContent = memo(function AsideList(props: { record: any }) {
-  if (!props.record) return <></>;
-  return (
-    <Box>
-      <List
-        queryOptions={{ meta: { allVersion: true, record: props.record } }}
-        actions={false}
-        disableSyncWithLocation={true}
-        pagination={<Pagination rowsPerPageOptions={[5, 10, 25]} />}
-      >
-        <Datagrid
-          rowClick="show"
-          rowSx={getStyle(props.record).rowSx}
-          bulkActionButtons={false}
+const BoxContent = memo(function BoxContent(props: { record: any }) {
+    if (!props.record) return <></>;
+    return (
+        <List
+            queryOptions={{ meta: { allVersion: true, record: props.record } }}
+            actions={false}
+            disableSyncWithLocation={true}
+            pagination={<Pagination rowsPerPageOptions={[5, 10, 25]} />}
         >
-          <TextField
-            source="metadata.version"
-            label="resources.common.version.version"
-          />
-          <DateField
-            source="metadata.created"
-            label="resources.common.version.created"
-          />
-        </Datagrid>
-      </List>
-    </Box>
-  );
+            <Datagrid
+                rowClick="show"
+                rowSx={getStyle(props.record).rowSx}
+                bulkActionButtons={false}
+            >
+                <TextField
+                    source="metadata.version"
+                    label="resources.common.version.version"
+                />
+                <DateField
+                    source="metadata.created"
+                    label="resources.common.version.created"
+                />
+            </Datagrid>
+        </List>
+    );
 }, arePropsEqual);
 
 type LayoutContentProps = {
-  children: ReactNode;
-  record: any;
+    children: ReactNode;
+    record: any;
 };
 
 export const TaskComponent = () => {
-  return <div>Json Scehma input</div>;
+    return <div>Json Scehma input</div>;
 };
 export const FunctionList = () => {
-  const record = useRecordContext();
-  const dataProvider = useDataProvider();
-  if (!record || !record.metadata || !dataProvider) return <></>;
-  return (
-    <List>
-      <Datagrid>
-        <TextField source="id" />
-        <TextField source="title" />
-      </Datagrid>
-    </List>
-  );
+    const record = useRecordContext();
+    const dataProvider = useDataProvider();
+    if (!record || !record.metadata || !dataProvider) return <></>;
+    return (
+        <List>
+            <Datagrid>
+                <TextField source="id" />
+                <TextField source="title" />
+            </Datagrid>
+        </List>
+    );
 };
 
 export const OutlinedCard = (props: { children }) => (
-  <Card variant="outlined" sx={{ width: "100%", borderRadius: "10px" }}>
-    {props.children}
-  </Card>
+    <Card variant="outlined" sx={{ width: '100%', borderRadius: '10px' }}>
+        {props.children}
+    </Card>
 );
--- src/resources/dataitems/show.tsx
+++ src/resources/dataitems/show.tsx
@@ -1,170 +1,187 @@
-import { JsonSchemaField } from "@dslab/ra-jsonschema-input";
-import { Grid, Typography } from "@mui/material";
-import * as changeCase from "change-case";
-import { ReactElement, memo, useEffect, useState } from "react";
+import { JsonSchemaField } from '@dslab/ra-jsonschema-input';
+import { Box, Grid, Typography } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import * as changeCase from 'change-case';
+import inflection from 'inflection';
+import { ReactElement, memo, useEffect, useState } from 'react';
 import {
-  Datagrid,
-  Labeled,
-  ListContextProvider,
-  Show,
-  TabbedShowLayout,
-  TextField,
-  useList,
-  useRecordContext,
-  useTranslate,
-} from "react-admin";
-import { MetadataSchema } from "../../common/types";
+    Datagrid,
+    Labeled,
+    ListContextProvider,
+    Show,
+    TabbedShowLayout,
+    TextField,
+    useList,
+    useRecordContext,
+    useTranslate
+} from 'react-admin';
+import { MetadataSchema } from '../../common/types';
 import {
-  LayoutContent,
-  OutlinedCard,
-  PostShowActions,
-} from "../../components/helper";
-import { getReactField, isFieldValid } from "./helper";
-import { DataItemSpecSchema, DataItemSpecUiSchema } from "./types";
+    LayoutContent,
+    OutlinedCard,
+    PostShowActions,
+} from '../../components/helper';
+import { getReactField, isFieldValid } from './helper';
+import { DataItemSpecSchema, DataItemSpecUiSchema } from './types';
 
 const arePropsEqual = (oldProps: any, newProps: any) => {
-  if (!newProps.record) return true;
-  return Object.is(oldProps.record, newProps.record);
+    if (!newProps.record) return true;
+    return Object.is(oldProps.record, newProps.record);
 };
 
 const ShowComponent = (props: { setRecord: (record: any) => void }) => {
-  const record = useRecordContext();
+    const record = useRecordContext();
 
-  useEffect(() => {
-    props.setRecord(record);
-  }, [record]);
+    useEffect(() => {
+        props.setRecord(record);
+    }, [record]);
 
-  return <DataItemShowLayout record={record} />;
+    return <DataItemShowLayout record={record} />;
 };
 
 const DataItemShowLayout = memo(function DataItemShowLayout(props: {
-  record: any;
+    record: any;
 }) {
-  const translate = useTranslate();
+    const translate = useTranslate();
 
-  if (!props.record) return <></>;
-  return (
-    <TabbedShowLayout syncWithLocation={false} record={props.record}>
-      <TabbedShowLayout.Tab label="resources.dataitem.tab.summary">
-        <Grid>
-          <Typography variant="h6" gutterBottom>
-            {translate("resources.dataitem.title")}
-          </Typography>
+    if (!props.record) return <></>;
+    return (
+        <TabbedShowLayout syncWithLocation={false} record={props.record}>
+            <TabbedShowLayout.Tab label="resources.dataitem.tab.summary">
+                <Grid>
+                    <Typography variant="h6" gutterBottom>
+                        {translate('resources.dataitem.title')}
+                    </Typography>
 
-          <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-            <Grid item xs={6}>
-              <Labeled label="resources.dataitem.name">
-                <TextField source="name" />
-              </Labeled>
-            </Grid>
-            <Grid item xs={6}>
-              <Labeled label="resources.dataitem.kind">
-                <TextField source="kind" />
-              </Labeled>
-            </Grid>
-          </Grid>
+                    <Grid container columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                        <Grid item xs={6}>
+                            <Labeled label="resources.dataitem.name">
+                                <TextField source="name" />
+                            </Labeled>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Labeled label="resources.dataitem.kind">
+                                <TextField source="kind" />
+                            </Labeled>
+                        </Grid>
+                    </Grid>
 
-          <JsonSchemaField source="metadata" schema={MetadataSchema} />
+                    <JsonSchemaField
+                        source="metadata"
+                        schema={MetadataSchema}
+                    />
 
-          <JsonSchemaField
-            source="spec"
-            schema={DataItemSpecSchema}
-            uiSchema={DataItemSpecUiSchema}
-            label="resources.dataitem.summary.spec.title"
-          />
-        </Grid>
-      </TabbedShowLayout.Tab>
-      <TabbedShowLayout.Tab label="resources.dataitem.tab.schema">
-        <SchemaTabComponent record={props.record} />
-      </TabbedShowLayout.Tab>
-      <TabbedShowLayout.Tab label="resources.dataitem.tab.preview">
-        <PreviewTabComponent record={props.record} />
-      </TabbedShowLayout.Tab>
-    </TabbedShowLayout>
-  );
+                    <JsonSchemaField
+                        source="spec"
+                        schema={DataItemSpecSchema}
+                        uiSchema={DataItemSpecUiSchema}
+                        label="resources.dataitem.summary.spec.title"
+                    />
+                </Grid>
+            </TabbedShowLayout.Tab>
+            <TabbedShowLayout.Tab label="resources.dataitem.tab.schema">
+                <SchemaTabComponent record={props.record} />
+            </TabbedShowLayout.Tab>
+            <TabbedShowLayout.Tab label="resources.dataitem.tab.preview">
+                <PreviewTabComponent record={props.record} />
+            </TabbedShowLayout.Tab>
+        </TabbedShowLayout>
+    );
 },
 arePropsEqual);
 
-export const DataItemShow = () => {
-  const [record, setRecord] = useState(undefined);
-
-  return (
-    <LayoutContent record={record}>
-      <Show
-        actions={<PostShowActions />}
-        sx={{ width: "100%" }}
-        component={OutlinedCard}
-      >
-        <ShowComponent setRecord={setRecord} />
-      </Show>
-    </LayoutContent>
-  );
-};
-
 const SchemaTabComponent = (props: { record: any }) => {
-  const schema = props.record?.spec?.schema || [];
-  const listContext = useList({ data: schema });
+    const schema = props.record?.spec?.schema || [];
+    const translate = useTranslate();
 
-  const map = new Map<string, ReactElement>();
-  schema
-    .filter((fieldDescriptor) => Object.keys(fieldDescriptor).length > 2)
-    .forEach((fieldDescriptor) => {
-      const filteredKeys = Object.keys(fieldDescriptor).filter(
-        (key) => key !== "name" && key !== "type"
-      );
-      filteredKeys.forEach((key, i) => {
-        if (!map.has(key)) {
-          map.set(
-            key,
-            <TextField source={key} key={`dataitem-schema-${key}-${i}`} />
-          );
-        }
-      });
-    });
+    const columns: GridColDef[] = [
+        {
+            field: 'name',
+            headerName: translate('resources.dataitem.schema.name'),
+        },
+        {
+            field: 'type',
+            headerName: translate('resources.dataitem.schema.type'),
+        },
+    ];
 
-  const reactFields = Array.from(map.values());
+    schema
+        .filter(fieldDescriptor => Object.keys(fieldDescriptor).length > 2)
+        .forEach(fieldDescriptor => {
+            const filteredKeys = Object.keys(fieldDescriptor).filter(
+                key => key !== 'name' && key !== 'type'
+            );
 
-  return (
-    <ListContextProvider value={listContext}>
-      <Datagrid bulkActionButtons={false}>
-        <TextField source="name" label="resources.dataitem.schema.name" />
-        <TextField source="type" label="resources.dataitem.schema.type" />
-        {reactFields && reactFields.map((field) => field)}
-      </Datagrid>
-    </ListContextProvider>
-  );
+            filteredKeys.forEach(key => {
+                if (!columns.some(r => r.field === key)) {
+                    const label = inflection.transform(
+                        key.replace(/\./g, ' '),
+                        ['underscore', 'humanize']
+                    );
+
+                    columns.push({
+                        field: key,
+                        headerName: label,
+                    });
+                }
+            });
+        });
+
+    const rows = schema.map((obj: any, i: number) => ({ id: i, ...obj }));
+
+    return (
+        <Box style={{ width: '100%' }}>
+            <DataGrid columns={columns} rows={rows} />
+        </Box>
+    );
 };
 
 const PreviewTabComponent = (props: { record: any }) => {
-  const schema = props.record?.spec?.schema || [];
-  const reactFields: ReactElement[] = [];
+    const schema = props.record?.spec?.schema || [];
+    const reactFields: ReactElement[] = [];
 
-  schema.forEach((fieldDescriptor: any, i: number) => {
-    reactFields.push(getReactField(fieldDescriptor, i));
-  });
-
-  const data: any[] = [];
-  const preview = props.record?.status?.preview || [];
-
-  preview.forEach((obj) => {
-    const field = changeCase.camelCase(obj.name);
-
-    const fieldDescriptor = schema.find((s) => s.name === obj.name);
-    const isValid = isFieldValid(fieldDescriptor);
-
-    obj.value.forEach((v: any, i: number) => {
-      const value = isValid ? v : undefined;
-      data[i] ? (data[i][field] = value) : (data[i] = { [field]: value });
+    schema.forEach((fieldDescriptor: any, i: number) => {
+        reactFields.push(getReactField(fieldDescriptor, i));
     });
-  });
 
-  const listContext = useList({ data });
+    const data: any[] = [];
+    const preview = props.record?.status?.preview || [];
 
-  return (
-    <ListContextProvider value={listContext}>
-      <Datagrid bulkActionButtons={false}>
-        {reactFields && reactFields.map((field) => field)}
-      </Datagrid>
-    </ListContextProvider>
-  );
+    preview.forEach(obj => {
+        const field = changeCase.camelCase(obj.name);
+
+        const fieldDescriptor = schema.find(s => s.name === obj.name);
+        const isValid = isFieldValid(fieldDescriptor);
+
+        obj.value.forEach((v: any, i: number) => {
+            const value = isValid ? v : undefined;
+            data[i] ? (data[i][field] = value) : (data[i] = { [field]: value });
+        });
+    });
+
+    const listContext = useList({ data });
+
+    return (
+        <ListContextProvider value={listContext}>
+            <Datagrid bulkActionButtons={false}>
+                {reactFields && reactFields.map(field => field)}
+            </Datagrid>
+        </ListContextProvider>
+    );
+};
+
+export const DataItemShow = () => {
+    const [record, setRecord] = useState(undefined);
+
+    return (
+        <LayoutContent record={record}>
+            <Show
+                actions={<PostShowActions />}
+                sx={{ width: '100%' }}
+                component={OutlinedCard}
+            >
+                <ShowComponent setRecord={setRecord} />
+            </Show>
+        </LayoutContent>
+    );
 };
