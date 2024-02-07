51d7317cdcbe5ee69a089780c145e0cd0c554327
Claudio Ballardini <claudio.ballardini01@gmail.com>
2024-02-07 11:43:55 +0100

    Fix theme issue

--- src/layout/MyLayout.tsx
+++ src/layout/MyLayout.tsx
@@ -1,45 +1,57 @@
-import { useLocation } from "react-router-dom";
-import { useMediaQuery, Theme } from "@mui/material";
-import { Layout } from "react-admin";
-import { MyAppBar } from "./MyAppBar";
-import { MyMenu } from "./MyMenu";
-import { MySidebar } from "./MySidebar";
-import { SxProps } from "@mui/material/styles";
+import { useLocation } from 'react-router-dom';
+import { useMediaQuery, Theme } from '@mui/material';
+import { Layout } from 'react-admin';
+import { MyAppBar } from './MyAppBar';
+import { MyMenu } from './MyMenu';
+import { MySidebar } from './MySidebar';
+import { SxProps } from '@mui/material/styles';
+import { useTheme } from '@mui/material/styles';
 
 export const MyLayout = (props: any) => {
-  const url = useLocation();
-  let style: SxProps = {};
-  const sharedStyle: SxProps = {
-    backgroundColor: "rgb(250, 250, 250)",
-    paddingX: 4,
-  };
-  const isLarge = useMediaQuery<Theme>((theme) => theme.breakpoints.down("lg"));
-  const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
-
-  if (url.pathname === "/") {
-    style = {
-      ...style,
-      position: "absolute",
-      marginLeft: "auto",
-      marginRight: "auto",
-      left: !isLarge && !isSmall ? "30%" : isLarge && !isSmall ? "20%" : "5%",
-      right: !isLarge && !isSmall ? "30%" : isLarge && !isSmall ? "20%" : "5%",
-      top: "35%",
-      ...sharedStyle,
+    const url = useLocation();
+    let style: SxProps = {};
+    const sharedStyle: SxProps = {
+        backgroundColor: 'rgb(250, 250, 250)',
+        paddingX: 4,
     };
-  } else {
-    style = { ...sharedStyle };
-  }
+    const theme = useTheme();
+    const isLarge = useMediaQuery(theme.breakpoints.down('lg'));
+    const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
 
-  return (
-    <Layout
-      {...props}
-      appBar={MyAppBar}
-      menu={MyMenu}
-      sidebar={MySidebar}
-      sx={{
-        "& .RaLayout-content": style,
-      }}
-    />
-  );
+    if (url.pathname === '/') {
+        style = {
+            ...style,
+            position: 'absolute',
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            left:
+                !isLarge && !isSmall
+                    ? '30%'
+                    : isLarge && !isSmall
+                    ? '20%'
+                    : '5%',
+            right:
+                !isLarge && !isSmall
+                    ? '30%'
+                    : isLarge && !isSmall
+                    ? '20%'
+                    : '5%',
+            top: '35%',
+            ...sharedStyle,
+        };
+    } else {
+        style = { ...sharedStyle };
+    }
+
+    return (
+        <Layout
+            {...props}
+            appBar={MyAppBar}
+            menu={MyMenu}
+            sidebar={MySidebar}
+            sx={{
+                '& .RaLayout-content': style,
+            }}
+        />
+    );
 };
