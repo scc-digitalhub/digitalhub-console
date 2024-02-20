98bff445e5486d556cff52c052a172355996e13a
Claudio Ballardini <claudio.ballardini01@gmail.com>
2024-02-20 11:48:29 +0100

    Fix icon path

--- src/resources/functions/list.tsx
+++ src/resources/functions/list.tsx
@@ -13,11 +13,12 @@ import {
 import yamlExporter from '@dslab/ra-export-yaml';
 import { DeleteWithConfirmButtonShowingName } from '../../components/helper';
 import { ListPageTitle } from '../../components/pageTitle';
-import { FunctionIcon } from './icon';
+import { FunctionIcon } from '../../components/icons';
 import { VersionsList } from '../../components/versionsList';
 import { useSchemaProvider } from '../../provider/schemaProvider';
 import { useEffect, useState } from 'react';
 import { RowButtonGroup } from '../../components/RowButtonGroup';
+import { Container } from '@mui/material';
 
 export const FunctionList = () => {
     const translate = useTranslate();
@@ -57,32 +58,34 @@ export const FunctionList = () => {
           ]
         : [];
     return (
-        <ListBase exporter={yamlExporter}>
-            <>
-                <ListPageTitle icon={<FunctionIcon fontSize={'large'} />} />
-                <ListView filters={postFilters}>
-                    <Datagrid
-                        rowClick="show"
-                        expand={<VersionsList />}
-                        expandSingle={true}
-                    >
-                        <TextField source="name" />
-                        <TextField source="kind" />
-                        <DateField
-                            source="metadata.updated"
-                            showDate={true}
-                            showTime={true}
-                        />
-                        {/* <div style={{ display: 'flex', justifyContent: 'end' }}> */}
-                        <RowButtonGroup>
-                            <ShowButton />
-                            <EditButton />
-                            <DeleteWithConfirmButtonShowingName />
-                        </RowButtonGroup>
-                        {/* </div> */}
-                    </Datagrid>
-                </ListView>
-            </>
-        </ListBase>
+        <Container maxWidth={false}>
+            <ListBase exporter={yamlExporter}>
+                <>
+                    <ListPageTitle icon={<FunctionIcon fontSize={'large'} />} />
+                    <ListView filters={postFilters}>
+                        <Datagrid
+                            rowClick="show"
+                            expand={<VersionsList />}
+                            expandSingle={true}
+                        >
+                            <TextField source="name" />
+                            <TextField source="kind" />
+                            <DateField
+                                source="metadata.updated"
+                                showDate={true}
+                                showTime={true}
+                            />
+                            {/* <div style={{ display: 'flex', justifyContent: 'end' }}> */}
+                            <RowButtonGroup>
+                                <ShowButton />
+                                <EditButton />
+                                <DeleteWithConfirmButtonShowingName />
+                            </RowButtonGroup>
+                            {/* </div> */}
+                        </Datagrid>
+                    </ListView>
+                </>
+            </ListBase>
+        </Container>
     );
 };
