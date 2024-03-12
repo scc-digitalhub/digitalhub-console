8ae445e107a8cab4def401a14026383b22aae1ce
Claudio Ballardini <claudio.ballardini01@gmail.com>
2024-03-12 11:37:04 +0100

    Checkout from main of types

--- src/resources/functions/types.ts
+++ src/resources/functions/types.ts
@@ -3,6 +3,27 @@ export enum FunctionTypes {
     NEFERTEM = 'nefertem',
     JOB = 'job',
 }
+export const TaskMap = {
+    dbt: ['transform'],
+    nefertem: ['profile', 'validate', 'metric', 'infer'],
+};
+export const getTaskByFunction = kind => {
+    return TaskMap[kind];
+};
+
+export const BlankSchema = {
+    $schema: 'http://json-schema.org/draft-07/schema',
+    type: 'object',
+    properties: {},
+};
+
+export const getFunctionSpec = (kind: string | undefined) => {
+    if (!kind) {
+        return BlankSchema;
+    }
+
+    return BlankSchema;
+};
 
 export const getFunctionUiSpec = (kind: string | undefined) => {
     if (!kind) {
