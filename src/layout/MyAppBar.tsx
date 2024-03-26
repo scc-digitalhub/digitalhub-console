06e12230b66f456a338e4cb0c22db52f2b873d5f
etomaselli <etomaselli199@gmail.com>
2024-03-26 11:44:32 +0100

    Temporary pagination in search results

--- src/layout/MyAppBar.tsx
+++ src/layout/MyAppBar.tsx
@@ -2,33 +2,40 @@ import {
     RootResourceSelectorMenu,
     useRootSelector,
 } from '@dslab/ra-root-selector';
-import { AppBar, CheckboxGroupInput, TextInput, useRedirect, useTranslate } from 'react-admin';
-import { Button, IconButton, Typography } from '@mui/material';
+import {
+    AppBar,
+    CheckboxGroupInput,
+    TextInput,
+    useRedirect,
+    useTranslate,
+} from 'react-admin';
+import { Button, Typography } from '@mui/material';
 import HomeIcon from '@mui/icons-material/Home';
 
-import {
-    SearchBar
-} from '@dslab/ra-search-bar';
+import { SearchBar } from '@dslab/ra-search-bar';
+
 const filters = [
-    <CheckboxGroupInput source="type" choices={[
-        { id: 'function', name: 'Function' },
-        { id: 'dataitem', name: 'DataItem' },
-        { id: 'artifact', name: 'Artifact' },
-    ]}
+    <CheckboxGroupInput
+        source="type"
+        choices={[
+            { id: 'function', name: 'Function' },
+            { id: 'dataitem', name: 'DataItem' },
+            { id: 'artifact', name: 'Artifact' },
+        ]}
         label="Type"
         key={1}
         defaultValue={[]}
         parse={v => {
             //v=['function', 'dataitem']
             //return type:(function OR dataitem)
-            return `type:(${v.join(' OR ')})`
+            return `type:(${v.join(' OR ')})`;
         }}
         format={v => {
             //v=type:(function OR dataitem)
             //return ['function', 'dataitem']
-            const startIndex = v.indexOf('(')
-            const endIndex = v.indexOf(')')
-            return v.substring(startIndex + 1, endIndex).split(' OR ')
+            const startIndex = v.indexOf('(');
+            const endIndex = v.indexOf(')');
+            return v.substring(startIndex + 1, endIndex).split(' OR ');
         }}
     />,
     <TextInput
@@ -39,9 +46,9 @@ const filters = [
         defaultValue=""
         parse={v => {
             if (!(v.startsWith('"') && v.endsWith('"'))) {
-                v = `"${v}"`
+                v = `"${v}"`;
             }
-            return 'metadata.name:' + v
+            return 'metadata.name:' + v;
         }}
         format={v => v.split(':')[1].split('"')[0]}
     />,
@@ -53,9 +60,9 @@ const filters = [
         defaultValue=""
         parse={v => {
             if (!(v.startsWith('"') && v.endsWith('"'))) {
-                v = `"${v}"`
+                v = `"${v}"`;
             }
-            return 'metadata.description:' + v
+            return 'metadata.description:' + v;
         }}
         format={v => v.split(':')[1].split('"')[0]}
     />,
@@ -67,28 +74,32 @@ const filters = [
         defaultValue=""
         parse={v => {
             if (!(v.startsWith('"') && v.endsWith('"'))) {
-                v = `"${v}"`
+                v = `"${v}"`;
             }
-            return 'metadata.version:' + v
+            return 'metadata.version:' + v;
         }}
         format={v => v.split(':')[1].split('"')[0]}
     />,
     <TextInput
-    label="Labels"
-    source="metadata.labels"
-    alwaysOn
-    key={5}
-    defaultValue=""
-    parse={v => {
-        return `metadata.labels:(${v.split(',').join(' AND ')})`
-    }}
-    format={v => {
-        const startIndex = v.indexOf('(')
-        const endIndex = v.indexOf(')')
-        return v.substring(startIndex + 1, endIndex).split(' AND ').join(',')
-    }}
-/>,
-]
+        label="Labels"
+        source="metadata.labels"
+        alwaysOn
+        key={5}
+        defaultValue=""
+        parse={v => {
+            return `metadata.labels:(${v.split(',').join(' AND ')})`;
+        }}
+        format={v => {
+            const startIndex = v.indexOf('(');
+            const endIndex = v.indexOf(')');
+            return v
+                .substring(startIndex + 1, endIndex)
+                .split(' AND ')
+                .join(',');
+        }}
+    />,
+];
+
 export const MyAppBar = () => {
     const { root: projectId } = useRootSelector();
     const redirect = useRedirect();
@@ -96,8 +107,7 @@ export const MyAppBar = () => {
     const translate = useTranslate();
 
     return (
-        <AppBar color="primary" elevation={0}
-        >
+        <AppBar color="primary" elevation={0}>
             <Typography
                 flex="1"
                 textOverflow="ellipsis"
@@ -108,11 +118,20 @@ export const MyAppBar = () => {
             >
                 {projectId}
             </Typography>
-            <Button color="inherit" onClick={() => redirect('/')} startIcon={<HomeIcon />}>
+            <Button
+                color="inherit"
+                onClick={() => redirect('/')}
+                startIcon={<HomeIcon />}
+            >
                 {translate('bar.backProjects')}
-             </Button>
-             
-            <SearchBar hintText="Search" to="searchresults" filters={filters} filterSeparator=":"></SearchBar>
+            </Button>
+
+            <SearchBar
+                hintText="Search"
+                to="searchresults"
+                filters={filters}
+                filterSeparator=":"
+            ></SearchBar>
             <RootResourceSelectorMenu source="name" showSelected={false} />
         </AppBar>
     );
