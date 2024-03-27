8307282a17077d03c0a514d86e1239ea8174e37a
etomaselli <etomaselli199@gmail.com>
2024-03-27 15:23:14 +0100

    Handled no results, styling

--- src/components/SearchList.tsx
+++ src/components/SearchList.tsx
@@ -14,11 +14,14 @@ import {
 import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 import TableChartIcon from '@mui/icons-material/TableChart';
 import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import Inbox from '@mui/icons-material/Inbox';
 import { useSearch } from '@dslab/ra-search-bar';
 import { useEffect, useState } from 'react';
 import { useRootSelector } from '@dslab/ra-root-selector';
 import { RowButtonGroup } from './RowButtonGroup';
-import { Container, Tooltip } from '@mui/material';
+import { Container, Tooltip, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { FlatCard } from './FlatCard';
 
 const mapTypes = {
     function: {
@@ -49,8 +52,6 @@ const mapTypes = {
 export const SearchList = () => {
     const { params: searchParams, setParams, provider } = useSearch();
     console.log('context', searchParams);
-    // const [page, setPage] = useState(1);
-    // const [perPage, setPerPage] = useState(5);
     const [total, setTotal] = useState(1000);
     const [results, setResults] = useState<any[]>([]);
     const [loading, setLoading] = useState(true);
@@ -104,6 +105,13 @@ export const SearchList = () => {
             });
     }, [provider, searchParams, root, page, perPage, sort, order]);
 
+    if (loading) return <Loading />;
+    if (error) return <Error />;
+    if (!results) return null;
+    if (results.length === 0) return <NoResults />;
+
+    console.log('results', total, results);
+
     //create Listcontext for pagination handling
     const listContext: ListControllerResult = {
         sort: { field: sort, order: order } as SortPayload,
@@ -133,33 +141,25 @@ export const SearchList = () => {
         hasPreviousPage: page > 1,
     };
 
-    if (loading) return <Loading />;
-    if (error) return <Error />;
-    if (!results) return null;
-
-    console.log('results', total, results);
-
     return (
         <ListContextProvider value={listContext}>
-            <Container>
-                <Datagrid
-                    bulkActionButtons={false}
-                    className="MuiPaper-root MuiPaper-elevation MuiPaper-rounded MuiPaper-elevation1 MuiCard-root RaList-content css-bhp9pd-MuiPaper-root-MuiCard-root"
-                >
-                    <IconResource />
-                    {/* <TextField source="type" label="Type"/> */}
-                    <TextField source="metadata.name" label="Name" />
-                    <TextField source="kind" label="Kind" />
-                    <TextField
-                        source="metadata.description"
-                        label="Description"
-                    />
-                    <TextField source="metadata.updated" label="Updated" />
-                    <RowButtonGroup>
-                        <ShowResourceButton />
-                    </RowButtonGroup>
-                </Datagrid>
-                <Pagination />
+            <Container maxWidth={false} sx={{ paddingTop: '18px', marginX: 0 }}>
+                <FlatCard sx={{ paddingY: '18px' }}>
+                    <Datagrid bulkActionButtons={false}>
+                        <IconResource />
+                        <TextField source="metadata.name" label="Name" />
+                        <TextField source="kind" label="Kind" />
+                        <TextField
+                            source="metadata.description"
+                            label="Description"
+                        />
+                        <TextField source="metadata.updated" label="Updated" />
+                        <RowButtonGroup>
+                            <ShowResourceButton />
+                        </RowButtonGroup>
+                    </Datagrid>
+                    <Pagination />
+                </FlatCard>
             </Container>
         </ListContextProvider>
     );
@@ -173,3 +173,46 @@ const IconResource = () => {
     const record = useRecordContext();
     return <>{mapTypes[record.type].icon}</>;
 };
+
+const NoResults = () => {
+    const emptyMessage = 'Your search gave no results';
+
+    return (
+        <Root className="RaList-noResults">
+            <div className={EmptyClasses.message}>
+                <Inbox className={EmptyClasses.icon} />
+                <Typography variant="h4" paragraph>
+                    {emptyMessage}
+                </Typography>
+            </div>
+        </Root>
+    );
+};
+
+const PREFIX = 'RaEmpty';
+
+export const EmptyClasses = {
+    message: `${PREFIX}-message`,
+    icon: `${PREFIX}-icon`,
+};
+
+const Root = styled('span', {
+    name: PREFIX,
+    overridesResolver: (props, styles) => styles.root,
+})(({ theme }) => ({
+    flex: 1,
+    [`& .${EmptyClasses.message}`]: {
+        textAlign: 'center',
+        opacity: theme.palette.mode === 'light' ? 0.5 : 0.8,
+        margin: '0 1em',
+        color:
+            theme.palette.mode === 'light'
+                ? 'inherit'
+                : theme.palette.text.primary,
+    },
+
+    [`& .${EmptyClasses.icon}`]: {
+        width: '9em',
+        height: '9em',
+    },
+}));
