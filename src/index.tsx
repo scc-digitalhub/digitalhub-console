f0fa474c88933cfc6317fe6d3746847a3da193b5
Matteo Saloni <matteo.saloni@gmail.com>
2024-04-02 17:55:55 +0200

    chore: read app name from global

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
-
 // set title
-document.title = import.meta.env.VITE_APP_NAME || 'DigitalHub';
+document.title =
+    (globalThis as any).VITE_APP_NAME ||
+    import.meta.env.VITE_APP_NAME ||
+    'DigitalHub';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
