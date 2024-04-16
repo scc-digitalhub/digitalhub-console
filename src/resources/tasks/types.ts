8062fec9e845160b51a429f929c3691a3ab3e95a
Matteo Chini <matteo.chini@gmail.com>
2024-04-16 17:09:46 +0200

    hiding labels for array

--- src/resources/tasks/types.ts
+++ src/resources/tasks/types.ts
@@ -221,6 +221,9 @@ export const taskSpecUiSchema = {
             affinity: {
                 'ui:widget': 'hidden',
             },
+            tolerations:{
+                'ui:widget': 'hidden',
+            },
             resources: {
                 cpu: {
                     'ui:ObjectFieldTemplate':CoreResourceFieldWidget,
@@ -276,21 +279,26 @@ export const taskSpecUiSchema = {
 
             envs: {
                 items: {
+                    'ui:title':'',
                     'ui:ObjectFieldTemplate':KeyValueFieldWidget,
                 },
             },
             node_selector: {
                 items: {
+                    'ui:title':'',
                     'ui:ObjectFieldTemplate':KeyValueFieldWidget,
                 },
             },
             labels: {
+                'ui:widget': 'hidden',
                 items: {
+                    'ui:title':'',
                     'ui:ObjectFieldTemplate':KeyValueFieldWidget,
                 },
             },
             volumes:{
                 items: {
+                    'ui:title':'',
                     'ui:ObjectFieldTemplate':VolumeResourceFieldWidget,
                     'ui:order': [ 'mount_path','name','volume_type','spec'],
 
