47fe1efb0e15fb61a455c1a26470cc2e6d8c7ade
Matteo Chini <matteo.chini@gmail.com>
2024-04-23 10:34:33 +0200

    work in progress with validator

--- src/components/resourceInput/KeyValueFieldWidget.tsx
+++ src/components/resourceInput/KeyValueFieldWidget.tsx
@@ -5,11 +5,14 @@ import {
   export const  KeyValueFieldWidget = (props: ObjectFieldTemplateProps) => {
       return (
           <> 
-       <div style={{ display: 'flex', width: '100%' }}><h3 style={{ width: '100%',textAlign:'center', }}>{props.title}</h3></div>
+          {props.title &&
+       <div style={{ display: 'flex', width: '100%' }}>
+        <h3 style={{ width: '100%',textAlign:'center', }}>{props.title}</h3>
+       </div>
+  }
         <div style={{ display: 'flex', width: '100%' }}>
-  
-          {props.properties.map((element) => (
-            <div style={{ width: '100%' , margin:'8px'}}>{element.content}</div>
+          {props.properties.map((element,index) => (
+            <div style={{ width: '100%' , margin:'0px 8px'}} key={index}>{element.content}</div>
             ))}
         </div>
         </>
