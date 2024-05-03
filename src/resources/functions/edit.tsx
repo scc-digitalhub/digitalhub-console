8497b122c26fbbadd15e996ea7a6668a5fa579d8
Matteo Chini <matteo.chini@gmail.com>
2024-05-03 13:28:20 +0200

    ace editor input first version

--- src/resources/functions/edit.tsx
+++ src/resources/functions/edit.tsx
@@ -3,17 +3,22 @@ import { Box, Container, Stack } from '@mui/material';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import deepEqual from 'deep-is';
-import { useEffect, useState } from 'react';
+import { Fragment, useEffect, useState } from 'react';
 import {
     Button,
     EditBase,
     EditView,
+    InputHelperText,
+    InputProps,
+    Labeled,
     LoadingIndicator,
+    RecordContextProvider,
     SaveButton,
     SelectInput,
     SimpleForm,
     TextInput,
     Toolbar,
+    useInput,
     useNotify,
     useRecordContext,
     useRedirect,
@@ -31,6 +36,31 @@ import { FunctionIcon } from './icon';
 import { getFunctionUiSpec } from './types';
 import { alphaNumericName } from '../../common/helper';
 import { JsonSchemaInput } from '../../components/JsonSchema';
+// import { spec } from 'node:test/reporters';
+// import { AceEditorInput } from '@dslab/ra-ace-editor';
+// import { useInput, InputProps, Labeled, InputHelperText } from 'react-admin';
+
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-java';
+import 'ace-builds/src-noconflict/mode-javascript';
+import 'ace-builds/src-noconflict/mode-markdown';
+import 'ace-builds/src-noconflict/mode-drools';
+import 'ace-builds/src-noconflict/mode-html';
+import 'ace-builds/src-noconflict/mode-python';
+import 'ace-builds/src-noconflict/mode-json';
+import 'ace-builds/src-noconflict/mode-sql';
+import 'ace-builds/src-noconflict/mode-typescript';
+import 'ace-builds/src-noconflict/mode-css';
+import 'ace-builds/src-noconflict/mode-yaml';
+import 'ace-builds/src-noconflict/mode-text';
+
+import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/theme-monokai';
+import 'ace-builds/src-noconflict/theme-solarized_dark';
+import 'ace-builds/src-noconflict/theme-solarized_light';
+// import { Fragment } from 'react';
+import React from 'react';
 
 export const FunctionEditToolbar = () => {
     const translate = useTranslate();
@@ -52,6 +82,103 @@ export const FunctionEditToolbar = () => {
     );
 };
 
+const Editor = (props: AceInputProps) => {
+    const {
+        mode = 'html',
+        theme = 'monokai',
+        useWorker = false,
+        //pick base from jsDelivr by default
+        basePath = 'https://cdn.jsdelivr.net/npm/ace-builds@',
+        label,
+        helperText,
+        fullWidth = false,
+        width = '50vw',
+        onBlur,
+        onChange,
+        resource,
+        source,
+    } = props;
+
+    const {
+        id,
+        field,
+        fieldState: { isTouched, error },
+        formState: { isSubmitted },
+        isRequired,
+    } = useInput({
+        // Pass the event handlers to the hook but not the component as the field property already has them.
+        // useInput will call the provided onChange and onBlur in addition to the default needed by react-hook-form.
+        onChange,
+        onBlur,
+        ...props,
+    });
+    const valueCode = atob(field?.value.base64 || '');
+    const labelProps = {
+        fullWidth,
+        isRequired,
+        label,
+        resource,
+        source,
+    };
+
+    //TODO let users customize options
+    const aceOptions = {
+        useWorker: useWorker,
+        showPrintMargin: false,
+    };
+    const onCodeChange =(data:string) => {
+        const encodedValue = btoa(data);
+        field.onChange({...field.value,base64:encodedValue});
+    }
+    // import workers (disabled by default)
+    // NOTE: this should match *exactly* the included ace version
+    // ace.config.set('basePath', basePath + ace.version + '/src-noconflict/');
+
+    return (
+        <Fragment>
+            <Labeled {...labelProps} id={id}> 
+                 <AceEditor
+                    // {...field}
+                    mode={mode}
+                    theme={theme}
+                    wrapEnabled
+                    width={fullWidth ? '100%' : width}
+                    setOptions={aceOptions}
+                    value={valueCode}
+                    onChange={onCodeChange}
+                /> 
+            </Labeled>
+            <InputHelperText
+                touched={isTouched || isSubmitted}
+                error={error?.message}
+                helperText={helperText}
+            />
+        </Fragment>
+    );
+};
+
+export type AceInputProps = InputProps & {
+    mode?:
+        | 'java'
+        | 'javascript'
+        | 'markdown'
+        | 'drools'
+        | 'html'
+        | 'python'
+        | 'json'
+        | 'sql'
+        | 'typescript'
+        | 'css'
+        | 'yaml'
+        | 'text';
+    //use a worker for syntax check
+    //disabled by default, loads from external cdn
+    useWorker?: boolean;
+    basePath?: string;
+    fullWidth?: boolean;
+    width?: string;
+    theme?: 'github' | 'monokai' | 'solarized_dark' | 'solarized_light';
+};
 const SpecInput = (props: {
     source: string;
     onDirty?: (state: boolean) => void;
@@ -61,6 +188,7 @@ const SpecInput = (props: {
     const resource = useResourceContext();
     const record = useRecordContext();
     const value = useWatch({ name: source });
+    const lang = useWatch({ name: 'spec.source.lang' });
     const eq = deepEqual(record[source], value);
 
     const schemaProvider = useSchemaProvider();
@@ -78,7 +206,10 @@ const SpecInput = (props: {
             onDirty(!eq);
         }
     }, [eq]);
+    
+    const onChange = (e) => {
 
+    }
     if (!record || !record.kind || !spec) {
         return (
             <Card
@@ -95,11 +226,14 @@ const SpecInput = (props: {
     }
 
     return (
+        <>
         <JsonSchemaInput
             source={source}
             schema={spec.schema}
             uiSchema={getFunctionUiSpec(record.kind)}
         />
+        <Editor source="spec.source"/>
+        </>
     );
 };
 
@@ -163,7 +297,11 @@ export const FunctionEdit = () => {
         });
         redirect('show', resource, data.id, data);
     };
+    const onChange = (e: any, id?:string) => {
+        console.log('called',e);
+        console.log('id',id);
 
+    };
     return (
         <Container maxWidth={false} sx={{ pb: 2 }}>
             <EditBase
@@ -203,6 +341,7 @@ export const FunctionEdit = () => {
                                     source="spec"
                                     onDirty={setIsSpecDirty}
                                 />
+     
                             </SimpleForm>
                         </FlatCard>
                     </EditView>
