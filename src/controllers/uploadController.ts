bd24a1125a198226f943393737527ce12edbc689
etomaselli <etomaselli199@gmail.com>
2024-06-25 13:17:43 +0200

    Upload controller

---
+++ src/controllers/uploadController.ts
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { AwsS3, Uppy } from "uppy";
+
+export const useUploadController = (props: any): UploadControllerResult => {
+    const { uppyProps, s3Props } = props;
+    const uppyConfig = uppyProps ? uppyProps : { restrictions: { maxNumberOfFiles: 1 } };
+    const s3Config = s3Props ? s3Props : {
+        id: 'myAWSPlugin',
+        shouldUseMultipart: false,
+    };
+    const [uppy] = useState(() =>
+        new Uppy(uppyConfig).use(AwsS3, s3Config)
+    );
+    return {
+        uppy
+    };
+};
+
+type UploadControllerResult = {
+    uppy: Uppy;
+};
