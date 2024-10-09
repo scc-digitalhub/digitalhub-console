33489e37ddeedcc841b855b4556816ab2834ae48
etomaselli <etomaselli199@gmail.com>
2024-10-09 12:07:30 +0200

    Round node for runs

--- src/components/lineageTab/CardNode.tsx
+++ src/components/lineageTab/CardNode.tsx
@@ -24,14 +24,14 @@ import { DataItemIcon } from '../../resources/dataitems/icon';
 import { RunIcon } from '../../resources/runs/icon';
 import { NODE_WIDTH } from './layouting';
 
-// store://prj1/model/model/testm1:4b32d511-b0bc-450a-bbdb-01b781d323c7
+/**
+ * Run key: store://prj1/run/python+run/cee60681-54a9-49b8-a69a-59ee079148a9
+ * Other keys: store://prj1/model/model/testm1:4b32d511-b0bc-450a-bbdb-01b781d323c7
+ */
 const parseKey = (key: string) => {
-    const [store, resourcePath, id] = key.split(':');
-    const resourceSplitted = resourcePath.split('/');
-    const name = resourceSplitted.pop();
-    const kind = resourceSplitted.pop();
-    const resource = resourceSplitted.pop() + 's';
-    return { resource, kind, name, id };
+    const [store, emptyStr, project, resource, kind, nameAndId] = key.split('/');
+    const [name, id] = nameAndId.split(':');
+    return { resource: resource + 's', kind, name, id: id || name };
 };
 
 // const calculateLabelWidth = () => {
@@ -60,62 +60,62 @@ export const CardNode = memo(function CardNode(props: {
         e.stopPropagation();
     };
 
-    const innerCard = (
-        <>
-            <CardHeader
-                avatar={icons[resource]}
-                title={'#' + name}
-                subheader={kind}
-            />
-            {/* <CardContent sx={{ p: '8px' }}>
-                {record && (
-                    <RecordContextProvider value={record}>
-                        {!data?.current && (
-                            <Stack sx={{ color: 'text.secondary' }}>
-                                <Labeled>
-                                    <DateField
-                                        showTime
-                                        source="metadata.updated"
-                                        label="fields.updated.title"
-                                        variant="caption"
-                                    />
-                                </Labeled>
-                            </Stack>
-                        )}
-                        <Stack direction={'row'}>
-                            {record.metadata.labels
-                                ?.slice(0, 3)
-                                .map((label, index) => {
-                                    if (index < 2) {
-                                        return (
-                                            <Chip
-                                                key={label}
-                                                label={label}
-                                                sx={{
-                                                    mr:
-                                                        index === 0
-                                                            ? '5px'
-                                                            : '0',
-                                                    mb: '8px',
-                                                    fontSize: 'xx-small',
-                                                    maxWidth:
-                                                        calculateLabelWidth(),
-                                                    overflow: 'hidden',
-                                                    textOverflow: 'ellipsis',
-                                                }}
-                                                size="small"
-                                            ></Chip>
-                                        );
-                                    } else {
-                                        return <span key={index}>...</span>;
-                                    }
-                                })}
-                        </Stack>
-                    </RecordContextProvider>
-                )}
-            </CardContent> */}
-        </>
-    );
+    // const innerCard = (
+    //     <>
+    //         <CardHeader
+    //             avatar={icons[resource]}
+    //             title={'#' + name}
+    //             subheader={kind}
+    //         />
+    //         <CardContent sx={{ p: '8px' }}>
+    //             {record && (
+    //                 <RecordContextProvider value={record}>
+    //                     {!data?.current && (
+    //                         <Stack sx={{ color: 'text.secondary' }}>
+    //                             <Labeled>
+    //                                 <DateField
+    //                                     showTime
+    //                                     source="metadata.updated"
+    //                                     label="fields.updated.title"
+    //                                     variant="caption"
+    //                                 />
+    //                             </Labeled>
+    //                         </Stack>
+    //                     )}
+    //                     <Stack direction={'row'}>
+    //                         {record.metadata.labels
+    //                             ?.slice(0, 3)
+    //                             .map((label, index) => {
+    //                                 if (index < 2) {
+    //                                     return (
+    //                                         <Chip
+    //                                             key={label}
+    //                                             label={label}
+    //                                             sx={{
+    //                                                 mr:
+    //                                                     index === 0
+    //                                                         ? '5px'
+    //                                                         : '0',
+    //                                                 mb: '8px',
+    //                                                 fontSize: 'xx-small',
+    //                                                 maxWidth:
+    //                                                     calculateLabelWidth(),
+    //                                                 overflow: 'hidden',
+    //                                                 textOverflow: 'ellipsis',
+    //                                             }}
+    //                                             size="small"
+    //                                         ></Chip>
+    //                                     );
+    //                                 } else {
+    //                                     return <span key={index}>...</span>;
+    //                                 }
+    //                             })}
+    //                     </Stack>
+    //                 </RecordContextProvider>
+    //             )}
+    //         </CardContent>
+    //     </>
+    // );
 
     return (
         <>
@@ -124,18 +124,47 @@ export const CardNode = memo(function CardNode(props: {
                 position={Position.Left}
                 style={{ background: '#555' }}
             />
-            <SquareNode
-                elevation={0}
-                className={data?.current ? 'MeNode' : 'RegularNode'}
-            >
-                {data?.current ? (
-                    innerCard
-                ) : (
-                    <CardActionArea onClick={handleCardClick}>
-                        {innerCard}
-                    </CardActionArea>
-                )}
-            </SquareNode>
+            {resource == 'runs' ? (
+                <RoundNode
+                    elevation={0}
+                    className={data?.current ? 'MeNode' : 'RegularNode'}
+                >
+                    {data?.current ? (
+                        <CardHeader
+                            avatar={icons[resource]}
+                            title={kind}
+                        />
+                    ) : (
+                        <CardActionArea onClick={handleCardClick}>
+                            <CardHeader
+                                avatar={icons[resource]}
+                                title={kind}
+                            />
+                        </CardActionArea>
+                    )}
+                </RoundNode>
+            ) : (
+                <SquareNode
+                    elevation={0}
+                    className={data?.current ? 'MeNode' : 'RegularNode'}
+                >
+                    {data?.current ? (
+                        <CardHeader
+                            avatar={icons[resource]}
+                            title={'#' + name}
+                            subheader={kind}
+                        />
+                    ) : (
+                        <CardActionArea onClick={handleCardClick}>
+                            <CardHeader
+                                avatar={icons[resource]}
+                                title={'#' + name}
+                                subheader={kind}
+                            />
+                        </CardActionArea>
+                    )}
+                </SquareNode>
+            )}
             <Handle
                 type="source"
                 position={Position.Right}
@@ -152,14 +181,15 @@ const icons = {
     runs: <RunIcon />,
 };
 
-const SquareNode = styled(Card, {
-    name: 'SquareNode',
+const Node = styled(Card, {
+    name: 'Node',
     overridesResolver: (_props, styles) => styles.root,
 })(({ theme, className }) => ({
     border: 'solid 1px #ccc',
-    backgroundColor: className == 'MeNode'
-        ? alpha(theme.palette?.primary?.main, 0.12)
-        : 'white',
+    backgroundColor:
+        className == 'MeNode'
+            ? alpha(theme.palette?.primary?.main, 0.12)
+            : 'white',
     ...theme.applyStyles('dark', {
         backgroundColor:
             className == 'MeNode'
@@ -170,4 +200,19 @@ const SquareNode = styled(Card, {
     ['& .MuiCardHeader-root']: { padding: '8px' },
     ['& .RaLabeled-label']: { fontSize: '0.6em' },
     ['& .MuiTypography-caption']: { fontSize: '0.7em' },
+}));
+
+const SquareNode = styled(Node, {
+    name: 'SquareNode',
+    overridesResolver: (_props, styles) => styles.root,
+})(({ theme, className }) => ({}));
+
+const RoundNode = styled(Node, {
+    name: 'SquareNode',
+    overridesResolver: (_props, styles) => styles.root,
+})(({ theme, className }) => ({
+    borderRadius: '100%',
+    aspectRatio: 1 / 1,
+    display: 'flex',
+    justifyContent: 'center',
 }));
