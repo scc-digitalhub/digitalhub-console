cd828d1e782c55fafc7ec48e84b7dd2d4c35c8eb
Matteo Chini <matteo.chini@gmail.com>
2024-12-03 14:14:24 +0100

    using new package for dialog create and edit

--- src/resources/projects/list.tsx
+++ src/resources/projects/list.tsx
@@ -126,6 +126,7 @@ const Toolbar = () => {
                         }
                     },
                 }}
+                closeOnClickOutside={false}
             >
                 <ProjectCreateForm />
             </CreateInDialogButton>
