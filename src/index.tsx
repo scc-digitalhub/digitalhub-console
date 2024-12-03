e64f2f2b1c52fc76ef2df79c1389f0eede5d1c76
Matteo Saloni <matteo.saloni@gmail.com>
2024-12-03 13:07:34 +0100

    feat: self-host fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import '@fontsource/open-sans/400.css';
+import '@fontsource/open-sans/500.css';
+import '@fontsource/open-sans/600.css';
+import '@fontsource/open-sans/700.css';
 
 // set title
 document.title =
