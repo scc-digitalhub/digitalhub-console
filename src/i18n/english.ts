6469ffd21e9e7ad4913d302b99f2f566a653efc0
etomaselli <etomaselli199@gmail.com>
2024-12-17 11:40:42 +0100

    fix: read strings even when containing keywords

--- src/i18n/english.ts
+++ src/i18n/english.ts
@@ -9,16 +9,17 @@ import * as states from './en/states.json';
 import * as messages from './en/messages.json';
 import * as actions from './en/actions.json';
 
+console.log('fields', fields);
 const translations = {
     ...englishMessages,
-    fields,
-    specs,
-    resources,
-    pages,
-    csv,
-    states,
-    messages,
-    actions,
+    fields: fields && 'default' in fields ? fields['default'] : fields,
+    specs: specs && 'default' in specs ? specs['default'] : specs,
+    resources: resources && 'default' in resources ? resources['default'] : resources,
+    pages: pages && 'default' in pages ? pages['default'] : pages,
+    csv: csv && 'default' in csv ? csv['default'] : csv,
+    states: states && 'default' in states ? states['default'] : states,
+    messages: messages && 'default' in messages ? messages['default'] : messages,
+    actions: actions && 'default' in actions ? actions['default'] : actions,
 };
 
 export default translations;
