c1ed5f124dd9ffad4db80bb0506320297c4c4c96
Matteo Chini <matteo.chini@gmail.com>
2025-01-09 14:58:40 +0100

    new metrics Tab for model v0.1

--- src/resources/models/metrics-table/MetricsTabComponent.tsx
+++ src/resources/models/metrics-table/MetricsTabComponent.tsx
@@ -1,9 +1,22 @@
-import { Box, Typography, alpha } from '@mui/material';
-import { DataGrid, enUS, itIT } from '@mui/x-data-grid';
+import {
+    Box,
+    Card,
+    CardContent,
+    CardHeader,
+    Grid,
+    Typography,
+    alpha,
+    useTheme,
+} from '@mui/material';
+import { enUS, itIT } from '@mui/x-data-grid';
 import { useLocaleState, useTranslate } from 'react-admin';
-import { Spinner } from '../../../components/Spinner';
-import { useDataGridController } from '../../../controllers/useDataGridController';
+import { LineChart } from '@mui/x-charts/LineChart';
+import { CounterBadge } from '../../../components/CounterBadge';
 
+export type Metric = {
+    name: string;
+    values: any;
+};
 export const MetricsTabComponent = (props: { record: any }) => {
     const { record } = props;
     const translate = useTranslate();
@@ -12,11 +25,14 @@ export const MetricsTabComponent = (props: { record: any }) => {
         locale && locale === 'it'
             ? itIT.components.MuiDataGrid.defaultProps.localeText
             : enUS.components.MuiDataGrid.defaultProps.localeText;
-
-    const { data, isLoading } = useDataGridController({
-        fields: record?.spec?.metrics,
-    });
-
+    const tmpMetrics = {
+        Loss: 45,
+        Accuracy: Array.from({ length: 10 }, () => Math.random()),
+        Precision: Array.from({ length: 10 }, () => Math.random()),
+        Recall: Array.from({ length: 10 }, () => Math.random()),
+        CPU: 0.32,
+        RAM: 200,
+    };
     return (
         <Box
             sx={{
@@ -27,35 +43,71 @@ export const MetricsTabComponent = (props: { record: any }) => {
                 {translate('resources.models.metrics.title')}
             </Typography>
 
-            {isLoading ? (
-                <Spinner />
-            ) : (
-                <DataGrid
-                    columns={data?.columns || []}
-                    rows={data?.rows || []}
-                    autoHeight
-                    hideFooter={
-                        data?.rows && data.rows.length > 100 ? false : true
-                    }
-                    localeText={localeText}
-                    sx={theme => ({
-                        '& .MuiDataGrid-columnHeader': {
-                            backgroundColor: alpha(
-                                theme.palette?.primary?.main,
-                                0.12
-                            ),
-                        },
-                        '& .MuiDataGrid-columnHeader, .MuiDataGrid-cell': {
-                            '&:not(:last-child)': {
-                                borderRight: '1px solid rgba(224, 224, 224, 1)',
-                            },
-                        },
-                        '& .MuiDataGrid-columnHeaderTitle': {
-                            fontWeight: 'bold',
-                        },
-                    })}
-                />
+            {tmpMetrics && (
+                <Grid container spacing={2} sx={{ paddingY: '16px' }}>
+                    {Object.entries(tmpMetrics).map(([key, value], index) => (
+                        <Grid item xs={12} md={4} key={'metric_' + index}>
+                            <MetricCard metric={{ name: key, values: value }} />
+                        </Grid>
+                    ))}
+                </Grid>
             )}
         </Box>
+    );
+};
+const MetricCard = (props: { metric: Metric }) => {
+    const { metric } = props;
+    const theme = useTheme();
+    const bgColor = alpha(theme.palette?.primary?.main, 0.08);
+
+    return (
+        <Card>
+            <CardHeader title={metric.name} />
+
+            <CardContent sx={{ paddingTop: 0 }}>
+                <Typography
+                    variant="body2"
+                    sx={{ height: '120px', overflowY: 'auto' }}
+                >
+                    {typeof metric.values === 'number' ? (
+                        <Box
+                            sx={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                height: '100%',
+                            }}
+                        >
+                            <CounterBadge
+                                value={metric.values}
+                                color="secondary.main"
+                                backgroundColor={bgColor}
+                                size="large"
+                            />
+                        </Box>
+                    ) : (
+                        <LineChart
+                            xAxis={[
+                                { data: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+                            ]}
+                            series={[
+                                {
+                                    data: metric.values,
+                                    label: metric.name,
+                                    showMark: false,
+                                },
+                            ]}
+                            slotProps={{ legend: { hidden: true } }}
+                            margin={{
+                                left: 24,
+                                right: 8,
+                                top: 8,
+                                bottom: 24,
+                            }}
+                        />
+                    )}
+                </Typography>
+            </CardContent>
+        </Card>
     );
 };
