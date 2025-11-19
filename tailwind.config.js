4d3c65b454ddbdfbf6d95a6b87c5aba03950713c
Sano <sanasar.hambardzumyan@gmail.com>
2025-11-19 12:54:52 +0100

    chore: add configuration files, update ui of the chat

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './index.html',
+        './src/**/*.{js,ts,jsx,tsx}',
+        './node_modules/reablocks/**/*.{js,jsx,ts,tsx}',
+        './node_modules/reachat/**/*.{js,jsx,ts,tsx}',
+    ],
+    plugins: [],
+};
