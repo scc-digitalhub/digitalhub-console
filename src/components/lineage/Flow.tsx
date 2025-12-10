2c03b7d327b3894a03210a25a48a5452e379ad71
etomaselli <etomaselli199@gmail.com>
2025-12-10 14:47:31 +0100

    feat: lineage downloadable as image

--- src/components/lineage/Flow.tsx
+++ src/components/lineage/Flow.tsx
@@ -14,10 +14,15 @@ import {
     Node,
     Edge,
     OnConnectStart,
+    Panel,
+    getViewportForBounds,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { ForwardCardNode, ReverseCardNode } from './CardNode';
 import { getLayoutedElements, RelationshipDirection } from './utils';
+import DownloadIcon from '@mui/icons-material/GetApp';
+import { IconButtonWithTooltip, useTranslate } from 'react-admin';
+import { toPng } from 'html-to-image';
 
 export const Flow = (props: {
     nodes: Node[];
@@ -83,8 +88,66 @@ export const Flow = (props: {
                 colorMode={theme.palette.mode}
             >
                 <Background />
-                <Controls />
+                <Controls position="bottom-right" />
+                <FlowDownloadButton />
             </ReactFlow>
         </Box>
+    );
+};
+
+const imageWidth = 1024;
+const imageHeight = 1500;
+function downloadImage(dataUrl) {
+    const a = document.createElement('a');
+    a.setAttribute('download', 'reactflow.png');
+    a.setAttribute('href', dataUrl);
+    a.click();
+}
+
+const FlowDownloadButton = () => {
+    const { getNodes, getNodesBounds } = useReactFlow();
+    const translate = useTranslate();
+    const theme = useTheme();
+
+    const onClick = () => {
+        // we calculate a transform for the nodes so that all nodes are visible
+        // we then overwrite the transform of the `.react-flow__viewport` element
+        // with the style option of the html-to-image library
+        const nodesBounds = getNodesBounds(getNodes());
+        const viewport = getViewportForBounds(
+            nodesBounds,
+            imageWidth,
+            imageHeight,
+            0.5,
+            2,
+            0
+        );
+
+        const el = document.querySelector('.react-flow__viewport');
+        if (el) {
+            toPng(el as HTMLElement, {
+                backgroundColor: theme.palette.background.paper,
+                width: imageWidth,
+                height: imageHeight,
+                style: {
+                    width: '' + imageWidth,
+                    height: '' + imageHeight,
+                    transform: `translate(${viewport.x}px, ${viewport.y}px) scale(${viewport.zoom})`,
+                },
+            }).then(downloadImage);
+        }
+    };
+
+    return (
+        <Panel position="top-right">
+            <IconButtonWithTooltip
+                color="secondary"
+                size="small"
+                label={translate('actions.download_image')}
+                onClick={onClick}
+            >
+                <DownloadIcon />
+            </IconButtonWithTooltip>
+        </Panel>
     );
 };
