8d0835671914063b48dc8d5f9c175ba58297ed56
Matteo Chini <matteo.chini@gmail.com>
2026-04-07 15:50:21 +0200

    removed from import resume button

--- src/pages/runs/show.tsx
+++ src/pages/runs/show.tsx
@@ -35,7 +35,6 @@ import { useEffect, useState } from 'react';
 import { useSchemaProvider } from '../../common/provider/schemaProvider';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { useNavigate } from 'react-router-dom';
-import { ResumeButton } from './components/ResumeButton';
 import { countLines } from '../../common/utils/helpers';
 import { functionParser } from '../../common/utils/parsers';
 import { IdField } from '../../common/components/fields/IdField';
