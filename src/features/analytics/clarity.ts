204f87a55d8b524dcbb3ffc19658b50a1f0d971c
Matteo Chini <matteo.chini@gmail.com>
2026-05-12 13:05:03 +0200

    load env key in a better way (global env)

--- src/features/analytics/clarity.ts
+++ src/features/analytics/clarity.ts
@@ -1,6 +1,9 @@
 import Clarity from '@microsoft/clarity';
 
-const clarityKey = import.meta.env.VITE_CLARITY_KEY;
+const clarityKey: string =
+    import.meta.env.REACT_APP_CLARITY_KEY ||
+    (globalThis as any).REACT_APP_CLARITY_KEY ||
+    (process.env.REACT_APP_CLARITY_KEY as string);
 
 if (clarityKey) {
     Clarity.init(clarityKey);
