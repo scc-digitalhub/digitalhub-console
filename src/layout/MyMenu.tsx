376ff410c9cb4b85eebae7b8e5c93da5b114bc83
matteo-s <matteo.saloni@gmail.com>
2026-06-12 14:04:48 +0200

    feat: menu sections

--- src/layout/MyMenu.tsx
+++ src/layout/MyMenu.tsx
@@ -7,19 +7,29 @@ import {
     MenuItemLink,
     useBasename,
     useGetResourceLabel,
+    useSidebarState,
+    useTranslate,
 } from 'react-admin';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { Box, Divider } from '@mui/material';
+import { Box, Divider, Popover, Stack, Typography } from '@mui/material';
 import { BrowserIcon } from '../features/files/fileBrowser/components/icon';
 import { ServiceIcon } from '../pages/services/icon';
 import { LineageIcon } from '../features/lineage/components/icon';
 import { ProjectIcon } from '../pages/projects/icon';
 import { UploadSafeLink } from './UploadSafeLink';
 import { HubIcon } from '../features/hub/components/HubIcon';
+import { ReactElement, useRef, useState } from 'react';
+import { fontSize } from '@mui/system';
+
+const enableHub: string =
+    (globalThis as any).REACT_APP_HUB_CATALOG_URL ||
+    (process.env.REACT_APP_HUB_CATALOG_URL as string) ||
+    false;
 
 export const MyMenu = () => {
     const basename = useBasename();
     const getResourceLabel = useGetResourceLabel();
+    const translate = useTranslate();
 
     return (
         <Menu
@@ -30,26 +40,53 @@ export const MyMenu = () => {
         >
             <Box flex={1}>
                 <Menu.DashboardItem />
+                {enableHub && (
+                    <MenuItemLink
+                        leftIcon={<HubIcon />}
+                        to={`${basename}/hub`}
+                        primaryText={'pages.hub.menu'}
+                    />
+                )}
+                <Divider />
+                <MenuHeader
+                    primaryText="pages.menu.catalog.header"
+                    helperText="pages.menu.catalog.subheader"
+                />
                 <Menu.ResourceItem name="artifacts" />
                 <Menu.ResourceItem name="dataitems" />
                 <Menu.ResourceItem name="models" />
                 <Menu.ResourceItem name="functions" />
                 <Menu.ResourceItem name="workflows" />
+                <Divider />
+                <MenuHeader
+                    primaryText="pages.menu.operations.header"
+                    helperText="pages.menu.operations.subheader"
+                />
                 <Menu.ResourceItem name="runs" />
                 <Menu.ResourceItem name="triggers" />
-                <Menu.ResourceItem name="secrets" />
-                <MenuItemLink
-                    leftIcon={<BrowserIcon />}
-                    to={`${basename}/files`}
-                    primaryText={'fields.files.title'}
-                />
                 <MenuItemLink
                     leftIcon={<ServiceIcon />}
                     to={`${basename}/services`}
                     primaryText={'pages.services.title'}
                 />
                 <Menu.ResourceItem name="containerimages" />
+                <Menu.ResourceItem name="secrets" />
                 <Divider />
+                <MenuHeader
+                    primaryText="pages.menu.repository.header"
+                    helperText="pages.menu.repository.subheader"
+                />
+                <MenuItemLink
+                    leftIcon={<BrowserIcon />}
+                    to={`${basename}/files`}
+                    primaryText={'fields.files.title'}
+                />
+
+                <Divider />
+                <MenuHeader
+                    primaryText="pages.menu.project.header"
+                    helperText="pages.menu.project.subheader"
+                />
                 <MenuItemLink
                     leftIcon={<SettingsIcon />}
                     to={`${basename}/config`}
@@ -60,11 +97,7 @@ export const MyMenu = () => {
                     to={`${basename}/lineage`}
                     primaryText={'pages.lineage.title'}
                 />
-                <MenuItemLink
-                    leftIcon={<HubIcon />}
-                    to={`${basename}/hub`}
-                    primaryText={'pages.hub.menu'}
-                />
+
                 <MenuItemLink
                     leftIcon={<ProjectIcon />}
                     to={'/projects'}
@@ -74,5 +107,90 @@ export const MyMenu = () => {
                 />
             </Box>
         </Menu>
+    );
+};
+
+const MenuHeader = (props: {
+    primaryText: string;
+    helperText?: string;
+    icon?: ReactElement;
+}) => {
+    const translate = useTranslate();
+    const [open, setOpen] = useSidebarState();
+    const [popoverOpen, setPopoverOpen] = useState(false);
+    const anchorRef = useRef(null);
+
+    const handlePopoverOpen = () => {
+        if (!popoverOpen) {
+            setPopoverOpen(true);
+        }
+    };
+    const handlePopoverClose = () => {
+        if (popoverOpen) {
+            setPopoverOpen(false);
+        }
+    };
+    const showPopover = !!props.helperText;
+    const popoverExtendedProps = showPopover
+        ? {
+              'aria-owns': popoverOpen ? 'mouse-over-popover' : '',
+              'aria-haspopup': true,
+              onMouseEnter: handlePopoverOpen,
+              onMouseLeave: handlePopoverClose,
+          }
+        : {};
+    return (
+        <Stack
+            ref={anchorRef}
+            direction={'row'}
+            columnGap={0}
+            alignItems={'flex-start'}
+        >
+            <Box sx={{ px: 2, pb: 1 }}>
+                {open && (
+                    <Typography
+                        variant="inherit"
+                        component="span"
+                        noWrap
+                        sx={{
+                            flexGrow: 1,
+                            textTransform: 'uppercase',
+                            fontSize: '90%',
+                            cursor: 'default',
+                        }}
+                        color="text.secondary"
+                        {...popoverExtendedProps}
+                    >
+                        {translate(props.primaryText)}
+                    </Typography>
+                )}
+            </Box>
+            <Popover
+                sx={{ pointerEvents: 'none' }}
+                open={popoverOpen}
+                anchorEl={anchorRef.current}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                onClose={handlePopoverClose}
+                disableRestoreFocus
+                slotProps={{
+                    paper: {
+                        variant: 'outlined',
+                        square: true,
+                        elevation: 0,
+                    },
+                }}
+            >
+                <Typography variant="body2" sx={{ p: 1 }}>
+                    {translate(props.helperText || '')}
+                </Typography>
+            </Popover>
+        </Stack>
     );
 };
