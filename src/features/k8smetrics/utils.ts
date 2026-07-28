4f6c9a4ebab4fcbed610bd60a7bc677ace146635
matteo-s <matteo.saloni@gmail.com>
2026-07-28 14:38:47 +0200

    feat: support different metric units

--- src/features/k8smetrics/utils.ts
+++ src/features/k8smetrics/utils.ts
@@ -18,46 +18,54 @@ export const formatMetricsValue = (name: string, value: any) => {
     if (raw === undefined || raw === null || Number.isNaN(raw)) {
         return '-';
     }
+
     if (name.toLowerCase().includes('cpu')) {
         //default to nanocores if no format is provided
         format ??= 'n';
-
-        if (format == 'n') {
-            //metrics for cpu are nanocores, but we want to display millicores
-            raw = Math.floor(raw * 1000);
-            raw = CPUResource.fromMillicores(raw).valueOf();
-            return raw > 1000 ? `${(raw / 1000).toFixed(1)}` : `${raw}m`;
-        }
-
-        if (format == 'm') {
-            //millicores, we want to display millicores
-            raw = Math.floor(raw);
-            raw = CPUResource.fromMillicores(raw).valueOf();
-            return raw > 1000 ? `${(raw / 1000).toFixed(1)}` : `${raw}m`;
-        }
-        if (format == 'seconds') {
-            //seconds == cores, we display as is
-            return `${raw}`;
-        }
     } else if (name.toLowerCase().includes('memory')) {
         //default to bytes if no format is provided
         format ??= 'bytes';
-
-        if (format == 'bytes') {
-            //metrics for memory are in bytes, but we want to display them in a human readable format
-            raw = Math.floor(raw);
-            raw = MemoryResource.fromBytes(raw).valueOf();
-            return prettyBytes(raw, 1);
-        }
     } else if (name.toLowerCase().includes('disk')) {
         //default to bytes if no format is provided
         format ??= 'bytes';
-        if (format == 'bytes') {
-            //metrics for disk are in bytes, but we want to display them in a human readable format
-            raw = Math.floor(raw);
-            raw = MemoryResource.fromBytes(raw).valueOf();
-            return prettyBytes(raw, 2);
-        }
+    }
+
+    if (format == 'n') {
+        //metrics for cpu are nanocores, but we want to display millicores
+        raw = Math.floor(raw * 1000);
+        raw = CPUResource.fromMillicores(raw).valueOf();
+        return raw > 1000 ? `${(raw / 1000).toFixed(1)}` : `${raw}m`;
+    }
+
+    if (format == 'm') {
+        //millicores, we want to display millicores
+        raw = Math.floor(raw);
+        raw = CPUResource.fromMillicores(raw).valueOf();
+        return raw > 1000 ? `${(raw / 1000).toFixed(1)}` : `${raw}m`;
+    }
+
+    if (format == 'seconds') {
+        //seconds == cores, we display as is
+        return `${raw}`;
+    }
+
+    if (format == 'bytes' || format == 'B') {
+        //metrics for memory are in bytes, but we want to display them in a human readable format
+        raw = Math.floor(raw);
+        raw = MemoryResource.fromBytes(raw).valueOf();
+        return prettyBytes(raw, 1);
+    }
+    
+    if (format == 'megabytes' || format == 'Mb' || format == 'MB') {
+        //metrics for memory are in megabytes, but we want to display them in a human readable format
+        raw = Math.floor(raw * 1024 * 1024);
+        raw = MemoryResource.fromBytes(raw).valueOf();
+        return prettyBytes(raw, 1);
+    }
+
+    if (format == 'percent') {
+        raw = Math.floor(raw);
+        return `${raw}%`;
     }
 
     //fallback as is for unknown
